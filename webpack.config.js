4649d83e26c05d58dbfccc3c555e8d04fd5ad001
gsilvapt <gustavosantaremsilva@gmail.com>
2018-12-27 19:22:34 +0000

    Adds updated content.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,40 @@
-var webpack = require('webpack');
-var path = require('path');
+let webpack = require('webpack');
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+	'faker', 'lodash', 'react', 'react-dom', 'react-input-range', 'react-redux', 'react-router',
+	'redux', 'redux-form', 'redux-thunk',
+]
 
 module.exports = {
-  entry: './src/index.js',
+	entry: {
+		bundle: './src/index.js', // Creates bundle with our project
+		vendor: VENDOR_LIBS, // Creates bundle with project's dependencies 
+	},
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+		filename: '[name].[chunkhash].js' // Build entry bundles and save them as the keys specified above.
+	},
+	module: {
+		rules: [
+			{
+				use: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			},
+			{
+				use: ['style-loader', 'css-loader'],
+				test: /\.css$/
+			}
+		]
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			names: ['vendor', 'manifest']
+		}),
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		})
+	]
 };
